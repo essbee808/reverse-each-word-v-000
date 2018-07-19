8ec4a8f0403bd6ce387ba18720bb5cbc2a494c57
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-19 06:33:05 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,10 @@ def reverse_each_word(string)
   array << string.split
   
   array.each do |word| #=> array
-    empty << "#{word}".reverse_each
+    word.each do |letter|
+      empty << "#{letter}".reverse
+      empty.join
+  end
 end
   return empty
 end
