43acccbc2370577f9ca84661c467f2b54743095e
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-19 05:26:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,5 +7,5 @@ def reverse_each_word(string)
   array << string.split
   
   #=> reverse each element within the array
-  array.each_with_index{|item, index| index.reverse}
+  array.each_with_index{|item, index| return "#{item}"}
 end
